a20a2088814cccdc96442a553a14516490ea13ac
lunanyan <chkong1998@gmail.com>
2019-07-21 12:45:26 +0900

    fix parser

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ async def on_message(message):
         # parsing command
         message_t = message.content.replace("루냥아 ", '')
         args = message_t.split(' ')
-        cmd = args[:1]
+        cmd = args[0]
         params = args[1:]
         # reacting to command
         await m_api.say(message, "message_t : " + message_t + "\nargs : " + str(args) + "\ncmd : " + str(cmd) + "\nparams : " + str(params))
