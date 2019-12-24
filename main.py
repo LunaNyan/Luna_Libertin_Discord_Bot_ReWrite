ca6268b56afe9c6de1f6a9319e9395c7a80c6a5d
lunanyan <chkong1998@gmail.com>
2019-12-24 16:49:36 +0900

    commit for progress

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 #!/usr/bin/python3
 
+import sys
 import discord, asyncio
+import configparser
 
 sys.path.append('./modules/')
 import m_db
 sys.path.append('../')
 
+conf = configparser.ConfigParser()
+
+try:
+    conf_path = "config.ini"
+    conf = configparser.ConfigParser()
+    conf.read(conf_path)
+    print("INFO    : Configuration file loaded")
+except Exception as e:
+    print("FATAL   : Couldn't load configuration file : " + str(e))
+    sys.exit(1)
+
+client = discord.Client()
+
+@client.event
+async def count_reset():
+    while True:
+        await asyncio.sleep(1800)
+        m_db.purge_counts()
+
+@client.event
+async def on_ready():
+    print('INFO    : Bot is ready to use.')
+    print("INFO    : Account : " + str(client.user.name) + "(" + str(client.user.id) + ")")
+    client.loop.create_task(count_reset())
+
+@client.event
+async def on_message(message):
+    try:
+        u_count = m_db.user_count(message)
+        g_count = m_db.guild_count(message)
+
+print("INFO    : connecting to Discord. Please Wait..")
+client.run(conf.get("config", "bot_token"))
--- modules/m_db.py
+++ modules/m_db.py
@@ -1,11 +1,21 @@
-import sys
+import sys, os.path
 import sqlite3, configparser
 
-#if __name__ == '__main__':
-#    sys.exit(1)
+if __name__ == '__main__':
+    sys.exit(1)
 
-db_obj = sqlite3.connect("db_main.dat")
-db = db_obj.cursor()
+db_path = "db/db_main.dat"
+
+if os.path.isfile(db_path):
+    db_obj = sqlite3.connect(db_path)
+    db = db_obj.cursor()
+else:
+    print("WARNING : DB file not found, creating new one")
+    db_obj = sqlite3.connect(db_path)
+    db = db_obj.cursor()
+    db.execute("CREATE TABLE user(id INTEGER NOT NULL, name TEXT, bio TEXT, createdat DATETIME, level INTEGER, disclosure_createdat INTEGER, disclosure_joinedat INTEGER, user_passive_available INTEGER);")
+    db.execute("CREATE TABLE guild(id INTEGER NOT NULL, name TEXT, bio TEXT, createdat DATETIME, disclosure_guild INTEGER, disclosure_user_joinedat INTEGER, user_passive_available INTEGER, guild_passive_available INTEGER, custom_head TEXT, custom_guild_passive_text TEXT, news_channel INTEGER, welcome_message_cid INTEGER welcome_message_text TEXT, farewell_message_cid INTEGER, farewell_message_text TEXT);")
+    db_execute("CREATE TABLE guild_custom_commands(gid INTEGER, command TEXT NOT NULL, reply TEXT NOT NULL, writed INTEGER NOT NULL);")
 
 temp_obj = sqlite3.connect(":memory:")
 temp = temp_obj.cursor()
@@ -18,7 +28,7 @@ def user_count(message):
         temp.execute("SELECT count FROM user_chats_count WHERE id = ?", (str(message.author.id)))
         count = temp.fetchone()[0]
         temp.execute("UPDATE user_chats_count SET count = ? WHERE id = ?", (str(int(count)+1), str(message.author.id)))
-        return int(count)
+        return int(count+1)
     except:
         temp.execute("INSERT INTO user_chats_count VALUES(?, ?);", (str(message.author.id)), '1')
         return 1
@@ -29,8 +39,12 @@ def guild_count(message):
         temp.execute("SELECT count FROM guild_chats_count WHERE id = ?", (str(message.guild.id)))
         count = temp.fetchone()[0]
         temp.execute("UPDATE guild_chats_count SET count = ? WHERE id = ?", (str(int(count)+1), str(message.guild.id)))
-        return int(count)
+        return int(count+1)
     except:
         temp.execute("INSERT INTO guild_chats_count VALUES(?, ?);", (str(message.guild.id)), '1')
         return 1
     temp_obj.commit()
+
+def purge_counts():
+    temp.execute("DELETE FROM user_chats_count")
+    temp.execute("DELETE FROM guild_chats_count")
